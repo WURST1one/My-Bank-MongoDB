22d3a1ef4be657d863d2ad81721186de986d0a24
DevSesi2 <henrique.marchini.souza@gmail.com>
2024-08-23 13:37:03 -0300

    gerando ID pro usuario

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
+
+/* Configurando a conexão das portas do Banco de dados */
 const app = express();
 const PORT = 5000;
 const MONGO_URI = 'mongodb://localhost:27017/mydatabase';
@@ -11,7 +13,7 @@ const MONGO_URI = 'mongodb://localhost:27017/mydatabase';
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,27 +23,50 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+/* Definindo ID para cada usuario criado */
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 }
+});
+const Counter = mongoose.model('Counter', counterSchema);
+
+/* Nome, id e email */
 const userSchema = new mongoose.Schema({
+  _id: Number,
   name: String,
   email: String,
 });
-
 const User = mongoose.model('User', userSchema);
 
+/* ID continuo  */
+async function getNextSequenceValue(sequenceName) {
+  const sequenceDocument = await Counter.findByIdAndUpdate(
+    sequenceName,
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true }
+  );
+  return sequenceDocument.seq;
+}
+
+/* Salvando usuario */
 app.post('/api/save', async (req, res) => {
   const { name, email } = req.body;
 
-  const newUser = new User({ name, email });
-
   try {
+    const newId = await getNextSequenceValue('user_id');
+
+    const newUser = new User({ _id: newId, name, email });
+
     await newUser.save();
     res.status(200).json({ message: 'User saved successfully!' });
+
+    /* Error ao criar usuario */
   } catch (error) {
     res.status(500).json({ error: 'Failed to save user' });
   }
 });
 
-// Inicia o servidor
+/* Porta do servidor: 5000 */
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
